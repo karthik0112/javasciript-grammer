b2972f450bcce78bfd1fc4a60a346d6f41dfa6da
karthik M <karthikprasanth348@gmail.com>
2021-12-27 22:00:16 +0530

    symbol

---
+++ chapter 5/template string.js
@@ -0,0 +1,7 @@
+let apple = 10;
+let name =  `There are ${apple}, apples in the basket`;
+console.log(name);
+let firstName = "John";
+let lastName = "Doe";
+let text = `Welcome ${firstName}, ${lastName}!`;
+console.log(text);
