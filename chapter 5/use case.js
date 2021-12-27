85a19924b14f273784a7862fda2b05a1cb3b2438
karthik M <karthikprasanth348@gmail.com>
2021-12-27 22:04:41 +0530

    template string

---
+++ chapter 5/use case.js
@@ -0,0 +1,7 @@
+for (let alerts =0;alerts<4;alerts++){
+    let one = (alerts==1);
+    let is = one ? "is" : "are";
+    let s = one == 1 ? "" : "s";
+    let message = `There ${is} ${alerts} alert${s}`;
+    console.log(message);
+}
