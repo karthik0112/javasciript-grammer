d152889ba518da655284c83d6a50711a3797da0f
karthik M <karthikprasanth348@gmail.com>
2021-12-27 21:26:04 +0530

    primitive types

---
+++ chapter 6/string.js
@@ -0,0 +1,7 @@
+typeof "text"; //string
+typeof "JavaScript book";//string
+typeof "price"+250;//string
+
+let text = new Text("hello.");//undefined
+typeof text;//object
+typeof text.valueOf();//string
