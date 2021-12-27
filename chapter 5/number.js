a3e0190b5066d17b651913b5fea0018749875522
karthik M <karthikprasanth348@gmail.com>
2021-12-27 21:44:09 +0530

    number

--- chapter 5/number.js
+++ chapter 5/number.js
@@ -2,7 +2,7 @@ typeof -1;
 typeof 5;
 typeof 7;
 
-let number = new number(7);//undefined
+let number = new Number(7);//undefined
 typeof number;//object
 typeof number.valueOf();//number
 console.log(number);
