7651cd97806e0d094734a7c4ed8190a24d884b4e
karthik M <karthikprasanth348@gmail.com>
2021-12-28 15:31:52 +0530

    ternary operator

--- chapter 8/ternary_operator.js
+++ chapter 8/ternary_operator.js
@@ -1,4 +1,9 @@
-const statement=35;
+/*const statement=35;
 let result=30;
 result=(result>=statement)?"pass":"fail";
+console.log(result);*/
+
+
+var a=100;
+const result=(a%2==0)?"even":"odd";
 console.log(result);
