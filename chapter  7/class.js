0f6ce6c95a283bbd84a468cda56bc80e4b206d33
karthik M <karthikprasanth348@gmail.com>
2021-12-28 09:50:55 +0530

    modified

---
+++ chapter  7/class.js
@@ -0,0 +1,15 @@
+ /*class cat {
+     let property = 20; //"Unexpexted token" Error
+     this.property = 2;
+    }*/
+    class cat {
+        constructor(){
+            let property = 1; //Ok: local variable 
+            this.something = 2;//Ok: object property
+        }
+        mehtod(){
+            console.log(this.property); //undefined
+            console.log(this.something);//1
+        }
+    }
+    
