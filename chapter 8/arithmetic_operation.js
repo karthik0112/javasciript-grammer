5adf2197bb5a9b8b72e3a48d6295c78bad54ceb1
karthik M <karthikprasanth348@gmail.com>
2021-12-28 14:28:29 +0530

    hoisting

---
+++ chapter 8/arithmetic_operation.js
@@ -0,0 +1,11 @@
+let variable = 1; // undefined
+variable + 2; //3
+variable - 1; //0
+variable / 2; //0.5
+variable * 5; //5
+variable * variable; //1
+variable++; //1
+variable++; //2
+variable++; //3
+variable--; //4
+console.log(variable);
