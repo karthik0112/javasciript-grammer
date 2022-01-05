d601d3999a35d8026d137c938b19b02a86963149
karthik M <karthikprasanth348@gmail.com>
2022-01-05 14:38:54 +0530

    question18

--- programes_E1.js/question18.js
+++ programes_E1.js/question18.js
@@ -1,8 +1,8 @@
 let n=5;
 let num = "";
 let count = 1;
-for (let i = 1; i < 5; i++) {
-  for (let j = 1; j <= 5 - i; j++) {
+for (let i = 1; i < n; i++) {
+  for (let j = 1; j <= n - i; j++) {
     num += " ";
   }
   for (let k = 0; k < 2*i-i; k++) {
