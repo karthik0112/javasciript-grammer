d22ab149585bd8960ba409c88a0dba6405fe28b6
karthik M <karthikprasanth348@gmail.com>
2022-01-06 12:03:40 +0530

    foreaach

--- programes_E1.js/for_each.js
+++ programes_E1.js/for_each.js
@@ -1,11 +1,15 @@
-const myAwesomeArray = [
+/*const myAwesomeArray = [
     { id: 1, name: "john" },
     { id: 2, name: "Ali" },
     { id: 3, name: "Mass" },
   ]
-myAwesomeArray.forEach(element => console.log(element.name))
+myAwesomeArray.forEach(element => console.log(element.name))*/
 
-/*const newArray = [5, 4, 3, 2, 1]
-newArray.map(x => x * x)*/
-  
+const newArray = [5, 4, 3, 2, 1]
+//let arr=newArray.map((item) => item * item);
+//console.log(arr);
+function item(x){
+  return x*x
+}
+  console.log(newArray.map(item));
  
