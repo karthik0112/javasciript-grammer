203d0eb3c6a483e5c13abf81466e5152e21bce1b
karthik M <karthikprasanth348@gmail.com>
2022-01-07 12:43:06 +0530

    chapter12

--- chapter12/arraycmp.js
+++ chapter12/arraycmp.js
@@ -1,4 +1,31 @@
-var a={id: 1, name: "edison"};
+let a = [1,2];
+let b = [1,2];
+let c = [5,5];
+function is_array(value) {
+    return typeof value.reduce=="function"&&
+           typeof value.filter=="function"&&
+           typeof value.map=="function"&&
+           typeof value.length=="number";
+}
+function arrcmp(a,b){
+    // one or more values are not arrays:
+    if(!(is_array(a)&&is_array(b)))
+     return false;
+   //Not equal by length
+   if(a.length != b.length)
+   return false;
+   //compare by value
+   for(let i=0;i<a.length;i++)
+   if(a[i]!==b[i])
+   return false;
+   //All test passed: array a and b are equal
+return true;
+
+}
+ console.log(arrcmp(a,b));//true
+ console.log(arrcmp(b,b));//true
+ console.log(arrcmp(a,c));//false
+ /*var a={id: 1, name: "edison"};
 var b={id: 1, name: "edison"};
 var c={id: 1, name: "edison"};
 var d={id: 1, name: "edison"};
@@ -16,5 +43,5 @@ function cmp(object1, object2) {
     return true;
   }
   console.log(cmp(a, c)); // => true
-  console.log(cmp(c, d)); // => false
+  console.log(cmp(c, d)); // => false*/
   
--- chapter12/arrayflat.js
+++ chapter12/arrayflat.js
@@ -2,3 +2,5 @@ let array = [1,2,3,4,5];
 number=(array.flatMap(x=>x*2));
 console.log(number);
 
+
+
