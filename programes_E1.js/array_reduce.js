68154bc0bcb7a54f4eecee58074154764aa6830b
karthik M <karthikprasanth348@gmail.com>
2022-01-18 15:40:20 +0530

    evaluation 1

--- programes_E1.js/array_reduce.js
+++ programes_E1.js/array_reduce.js
@@ -1,5 +1,5 @@
 var people = [{id: 1, name: 'edison' },{id: 2, name: 'Annand'}, 
-{id: 3, name: 'Vasnath'}];
+{id: 3, name: 'Vasnath'},{id:1,name:'edison'}];
 let groupbyid = people.reduce((acc, curr) => {
   if(acc[curr.id]){
   acc[curr.id]=++acc[curr.id]
