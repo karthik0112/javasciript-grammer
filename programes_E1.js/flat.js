17bb2d4c31878d6b91f701e56cbaad0b8cddab18
karthik M <karthikprasanth348@gmail.com>
2022-01-25 10:28:07 +0530

    evaluation 1

--- programes_E1.js/flat.js
+++ programes_E1.js/flat.js
@@ -1,8 +1,14 @@
 //const multi = [1,4,2,3,[10,20,20], [100,500,700,600],
 //[2000,1000]];
 //console.log(multi.flat(Infinity));
-let array = [1,4,2,3,[10,20,20], [100,500,700,600],
+/*let array = [1,4,2,3,[10,20,20], [100,500,700,600],
 [2000,1000]];
 
 let newArr = array.concat(); 
-console.log(newArr);
+console.log(newArr);*/
+const arr =[1,4,2,3,[10,20,20], [100,500,700,600],[2000,1000]]  
+
+// To flat single level array
+// is equivalent to
+arr.reduce((acc, val) => acc.concat(val), []);
+console.log(arr);
